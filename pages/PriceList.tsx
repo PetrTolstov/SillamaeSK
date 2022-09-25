9c8129b5343b5425e9d8c22e4c274ce4c69f09da
Ivan Niukanen <ivanniu2003@gmail.com>
2022-09-25 14:58:27 +0300

    Query example (PriceList)

--- pages/PriceList.tsx
+++ pages/PriceList.tsx
@@ -1,14 +1,26 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 import Layout from "../components/Layout";
 import AppIsBeingBuilt from "../components/Temporary/AppIsBeingBuilt";
+import { useGetPriceListQuery } from "../graphqlGenerated/graphql";
 
 const PriceList: NextPage = () => {
-    return (
-        <Layout>
-            <AppIsBeingBuilt isEst={true}/>
-        </Layout>
-    )
-}
+	const { loading, error, data } = useGetPriceListQuery();
+	return (
+		<Layout>
+			<AppIsBeingBuilt isEst={true} />
+			<div>
+				{loading ? <p>Loading...</p> : <></>}
+				{data?.GetPriceList.map((el) => (
+					<div key={el?._id}>
+						<h1>{el?.name.ENG}</h1>
+						<h1>{el?.name.EST}</h1>
+						<h1>{el?.name.RUS}</h1>
+					</div>
+				))}
+			</div>
+		</Layout>
+	);
+};
 
-export default PriceList
+export default PriceList;
