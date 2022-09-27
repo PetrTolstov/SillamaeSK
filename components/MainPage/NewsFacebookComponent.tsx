8950518bd8866ef1b1981e55fada13dc693ec16b
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-09-27 14:43:43 +0300

    fd

--- components/MainPage/NewsFacebookComponent.tsx
+++ components/MainPage/NewsFacebookComponent.tsx
@@ -5,7 +5,6 @@ import {useState} from "react";
 function NewsFacebookComponent(){
     const [latestNews, setLatestNews] = useState({message: "", created_time: (new Date).toString()})
 
-
     fetch('https://graph.facebook.com/v15.0/spordikompleksKalev/feed?access_token=EAAVrZCSeKcAIBAP6QoLv91fVHT7VfbItJIkXIwngjcFFN6n1i7WZAdPkrA4KVD6thcJIhujXtY9q4ZAexENDMZBnJMOk7hOThR3tpP9a7sDZCLFU3LE4YIJBBOMDCEAyds04O2xIHcjHPIKqgU9BZAScLPJ0Kl1IRmG91ZBUyeTtN2N7ARyzqfLfoDL434DMl0ZD')
         .then(response => response.json())
         .then(data => {
