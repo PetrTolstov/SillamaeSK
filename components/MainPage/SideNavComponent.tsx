d7ae5b482f8bf6837fdf5cd63a696b37def5115b
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-09-29 03:25:19 +0300

    fixed sideNav

--- components/MainPage/SideNavComponent.tsx
+++ components/MainPage/SideNavComponent.tsx
@@ -1,18 +1,48 @@
 import styles from '../../styles/SideNavComponent.module.css'
+import Link from "next/link";
 
 function SideNavComponent(){
     return(
         <nav className={styles.sideNav}>
-            <a>Kunstmurustaadion</a>
-            <a>Ujula</a>
-            <a>Kergejõustikumaneež</a>
-            <a>Staadion</a>
+            <Link href={`/SportOpportunities/Kunstmurustaadion`}>
+                <a>Kunstmurustaadion</a>
+            </Link>
+            <Link href={`/SportOpportunities`}>
+                <a>Ujula</a>
+            </Link>
+            <Link href={`/SportOpportunities/Kergejoustikumaneez`}>
+                <a>Kergejõustikumaneež</a>
+            </Link>
+            <Link href={`/SportOpportunities/Staadion`}>
+                <a>Staadion</a>
+            </Link>
 
-            <a>Fitnessi saal</a>
-            <a>Suur saal</a>
-            <a>Väike saal</a>
-            <a>Jõusaal</a>
-            <a>Maleruum</a>
+            <Link href={`/SportOpportunities/FitnessiSaal`}>
+                <a>Fitnessi saal</a>
+            </Link>
+            <Link href={`/SportOpportunities/SuurSaal`}>
+                <a>Suur saal</a>
+            </Link>
+            <Link href={`/SportOpportunities/VaikeSaal`}>
+                <a>Väike saal</a>
+            </Link>
+            <Link href={`/SportOpportunities/Jousaal`}>
+                <a>Jõusaal</a>
+            </Link>
+            <Link href={`/SportOpportunities/Maleruum`}>
+                <a>Maleruum</a>
+            </Link>
+
+
+
+
+
+
+
+
+
+
+
         </nav>
     )
 }
