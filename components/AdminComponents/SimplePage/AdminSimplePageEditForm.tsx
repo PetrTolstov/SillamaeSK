864b2a033b730f97d68b5dac4a85a53079b01ce6
Ivan Niukanen <ivanniu2003@gmail.com>
2022-09-29 13:37:21 +0300

    Spordikomplesist adminaccess

--- components/AdminComponents/SimplePage/AdminSimplePageEditForm.tsx
+++ components/AdminComponents/SimplePage/AdminSimplePageEditForm.tsx
@@ -1,27 +1,46 @@
-import { ChangeEvent, useState } from "react";
-import { SimplePage, SimplePageInput } from "../../../graphqlGenerated/graphql";
+import { ChangeEvent, useEffect, useState } from "react";
+import {SimplePage, useEditSimplePageMutation, GetSimplePagesDocument } from "../../../graphqlGenerated/graphql";
 
-export const AdminSimplePageEditForm = ({page} : {page :SimplePage}) => {
-    const [ data, setData ] = useState<SimplePageInput>();  
 
-    const handleChange = (e: ChangeEvent) => { 
-        const dataCopy = data; 
-        // dataCopy?[e.currentTarget.id as keyof SimplePageInput]?[e.currentTarget.name] = " ggegadf" 
-    }
+export const AdminSimplePageEditForm = ({ page }: { page: SimplePage }) => {
+    const [ currentPage, setCurrentPage] = useState(page); 
+    const [editSimplePage, {loading, data, error}] = useEditSimplePageMutation();
+    useEffect(() => {
+        console.log(page);
+        setCurrentPage(page)
+    }, [page])
 	return (
 		<div>
-            <h3>{page?.title?.EST ?? ""}</h3>
-			<form style={{display: "flex", flexDirection: "column"}} onSubmit={() => {}}>
+			<h3>{page?.title?.EST ?? ""}</h3>
+			<form style={{ display: "flex", flexDirection: "column" }} onSubmit={(e) => {
+                e.preventDefault();
+                const formData = new FormData(e.currentTarget); 
+                editSimplePage({variables: {id: page._id, updatedSimplePage: { 
+                    title: {
+                        EST: formData.get("titleEST")?.toString(), 
+                        RUS: formData.get("titleRUS")?.toString(), 
+                    }, 
+                    text: { 
+                        EST: formData.get("textEST")?.toString(), 
+                        RUS: formData.get("textRUS")?.toString(), 
+                    },
+                    image: ""
+                }}, onCompleted() {
+                    alert("success");
+                }, refetchQueries: [{query: GetSimplePagesDocument}]})
+                location.reload();
+            }}>
 				<label>
 					Title
-                    <input id="title" name="EST" placeholder='Title EST' />
-					<input id="title" name="RUS" placeholder='Title RUS' />
+					<input key={currentPage?._id + " 1"} id='title' name='titleEST' placeholder='Title EST' defaultValue={page?.title?.EST ?? ""} />
+					<input key={currentPage?._id + " 2"} id='title' name='titleRUS' placeholder='Title RUS' defaultValue={page?.title?.RUS ?? ""} />
 				</label>
-                <label style={{display: "flex", flexDirection: "column"}}>
-                    Text
-                    <textarea placeholder="Text EST" cols={30} rows={5}></textarea>
-                    <textarea placeholder="Text RUS" cols={30} rows={5}></textarea>
-                </label>
+				<label style={{ display: "flex", flexDirection: "column" }}>
+					Text
+					<textarea placeholder='Text EST' name="textEST" cols={30} rows={10} defaultValue={page?.text?.EST ?? ""} ></textarea>
+					<textarea placeholder='Text RUS' name="textRUS" cols={30} rows={10} defaultValue={page?.text?.RUS ?? ""} ></textarea>
+				</label>
+                <input style={{width: "fit-content"}} type="submit" />
 			</form>
 		</div>
 	);
--- pages/AdminAccess/FromSportComplex.tsx
+++ pages/AdminAccess/FromSportComplex.tsx
@@ -6,6 +6,7 @@ import { SimplePage, useGetSimplePagesLazyQuery, useGetSimplePagesQuery } from "
 import { NextPageWithLayout } from "../_app";
 
 const FromSportComplex: NextPageWithLayout = () => {
+    const [currentPage, setCurrentPage] = useState<SimplePage>();
 	const { loading, data, error } = useGetSimplePagesQuery({
 		variables: { type: 0 },
 		onError(error) {
@@ -15,13 +16,12 @@ const FromSportComplex: NextPageWithLayout = () => {
 			setCurrentPage(data.GetSimplePages![0] as SimplePage);
 		},
 	});
-	const [currentPage, setCurrentPage] = useState<SimplePage>();
+	
 
 	return (
 		<div>
 			<h1>Sportkompleksist</h1>
 			{loading ? <p>loading...</p> : <></>}
-			<p>{"Current Page " + currentPage?.title?.EST}</p>
 			<AdminDropDown
 				pages={data?.GetSimplePages as SimplePage[]}
 				currentPage={currentPage as SimplePage}
