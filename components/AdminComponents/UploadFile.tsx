d99cf884fc55e1b11737005c38b0c353eb123421
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-10-03 13:29:56 +0300

    admin design

--- components/AdminComponents/UploadFile.tsx
+++ components/AdminComponents/UploadFile.tsx
@@ -3,8 +3,9 @@ import axios from "axios";
 import { LINK } from "../../config/constants";
 import styles from "../../styles/ModalAdmin.module.css";
 import { ButtonAdmin } from "./ButtonAdmin";
+import kalender from "../../pages/Kalender";
 
-const ImageForm = ({ page, show, closeModal }: { page: string; show: boolean, closeModal: ()=>void }) => {
+const ImageForm = ({ page, show, closeModal}: { page: string; show: boolean, closeModal: ()=>void }) => {
 	//const page = '/Gallery'
 
 	const [file, setFile] = useState<FileList>();
@@ -12,7 +13,10 @@ const ImageForm = ({ page, show, closeModal }: { page: string; show: boolean, cl
 
 	const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setFile(event.currentTarget.files!);
+		setImgFile(URL.createObjectURL(event.currentTarget.files![0]))
 	};
+
+
 
 	useEffect(() => {
 		(async () => {
@@ -21,14 +25,19 @@ const ImageForm = ({ page, show, closeModal }: { page: string; show: boolean, cl
 					optional: page,
 				},
 			});
-			setImgFile(`${LINK}/public/images/${page}/${res.data[0]}`);
+			//setImgFile(`${LINK}/public/images/${page}/${res.data[0]}`);
 		})();
 	}, []);
 
 	const handleSubmit = (event: { preventDefault: () => void }) => {
+		if(!file){
+			alert("Valed andmed")
+		}
+
 		event.preventDefault();
 		const data = new FormData();
 		data.append("optional", page);
+		console.log(file)
 		if (file) {
 			// @ts-ignore
 			for (let x = 0; x < file.length; x++) {
@@ -44,22 +53,23 @@ const ImageForm = ({ page, show, closeModal }: { page: string; show: boolean, cl
                 <div className={styles.shadow}></div>
 				<div className={styles.container}>
                     <ButtonAdmin border action={closeModal} label={"Close modal"} />
-					<form>
-						<div className='form-group'>
+					<form onSubmit={handleSubmit} style={{marginBottom: "50px", marginTop: "50px"}}>
+						<div className='form-group' >
 							<label htmlFor='file'>Upload File:</label>
+							<br/>
 							<input
 								className='form-control-file mb-3'
 								type='file'
 								id='file'
 								multiple
 								onChange={handleFileChange}
+
 							/>
-							<button onClick={handleSubmit}>
-								Upload
-							</button>
+							<ButtonAdmin filled isSubmit label={"Upload"} action={() => {}}/>
+
 						</div>
 					</form>
-					<img src={imgFile} alt='img' />
+					{imgFile ? <img src={imgFile} alt='img' style={page == "Karusel" ? {width: "100%", height: "300px",  backgroundSize: '100% 100%', borderRadius: "10px"} : {} }/> : <p>Eelvaate nägemiseks lisage foto</p> }
 					{/* Display Image Here */}
 				</div>
 			</>
--- pages/AdminAccess/MainPageAdmin.tsx
+++ pages/AdminAccess/MainPageAdmin.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import {ReactElement, useState} from "react";
 import { AdminLayout } from ".";
 import { NextPageWithLayout } from "../_app";
 import { TimeTableEditting } from "../../components/AdminComponents/TimeTableEditting";
@@ -7,14 +7,22 @@ import AdminStore from "../../Stores/AdminStore";
 import { observer } from "mobx-react-lite";
 import Link from "next/link";
 import { GoBackPage } from "../../components/AdminComponents/GoBackPage";
+import ImageForm from "../../components/AdminComponents/UploadFile";
+import {ButtonAdmin} from "../../components/AdminComponents/ButtonAdmin";
 
 const MainPageAdmin: NextPageWithLayout = () => {
+	const [isShow, setIsShow] = useState(false)
+
 	if (AdminStore.userInfo.isLoggedIn) {
 		return (
 			<div style={{ paddingBottom: "50px" }}>
 				<h1>Avaleht</h1>
 				<TimeTableEditting />
 				<SportOpportunitesDesEdititng />
+				<h3>Lisa foto</h3>
+				<ImageForm page={"Karusel"} show={isShow} closeModal={() => {setIsShow(false)}}/>
+				<ButtonAdmin border label={'Lisa'} action={() => {setIsShow(true)}}/>
+
 			</div>
 		);
 	} else {
