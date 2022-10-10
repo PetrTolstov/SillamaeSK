47778e521f64dd42a45bc2db54e43f5d85dcd5a8
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-10-10 04:37:21 +0300

    gallery

--- components/AdminComponents/deleteFile.tsx
+++ components/AdminComponents/deleteFile.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, {ChangeEvent, ChangeEventHandler, useEffect, useState} from "react";
 import axios from "axios";
 import { LINK } from "../../config/constants";
 import styles from "../../styles/ModalAdmin.module.css";
@@ -8,7 +8,7 @@ import stylesBut from "../../styles/ButtonAdmin.module.css";
 
 const DeleteImage = ({ page, show, closeModal}: { page: string; show: boolean, closeModal: ()=>void }) => {
 	const [imgFile, setImgFile] = useState([]);
-
+	const [listImgDel, setListImgDel] = useState([])
 
 	useEffect(() => {
 		(async () => {
@@ -21,6 +21,54 @@ const DeleteImage = ({ page, show, closeModal}: { page: string; show: boolean, c
 		})();
 	}, []);
 
+
+	const handleTitleChange = (event: any) => {
+		const select = document.getElementById('select')
+		// @ts-ignore
+		var value = select.options[select.selectedIndex].value;
+		Array.from(document.getElementsByClassName(styles.imgCont)).forEach((el) => {
+			if (!(styles.hidden in el.classList)){
+				el.classList.add(styles.hidden)
+			}
+		})
+		document.getElementById(value)!.classList.remove(styles.hidden)
+	};
+
+	const handleSubmit = (event: { preventDefault: () => void }) => {
+
+		event.preventDefault();
+
+
+		if (listImgDel) {
+
+			axios.post(LINK + "/delete", listImgDel).then((r) => closeModal());
+		}else{
+			alert("Choose photo to remove")
+		}
+
+	};
+
+
+	const handleImgClick = (event: any) => {
+		console.log(event.target.id)
+		const img : string = event.target.id
+		let copyList : string[] = [...listImgDel]
+
+		if (copyList.indexOf(img) != -1){
+			document.getElementById(img)?.classList.remove(styles.chosen)
+
+			copyList = copyList.filter(data => data != img)
+
+		}else{
+			document.getElementById(img)?.classList.add(styles.chosen)
+			copyList.push(img)
+
+		}
+		console.log(copyList)
+		// @ts-ignore
+		setListImgDel(copyList)
+
+	};
 	/*const handleSubmit = (event: { preventDefault: () => void }) => {
 		if(!file){
 			alert("Valed andmed")
@@ -46,10 +94,31 @@ const DeleteImage = ({ page, show, closeModal}: { page: string; show: boolean, c
 				<div className={styles.shadow}></div>
 				<div className={styles.container}>
 					<ButtonAdmin border action={closeModal} label={"Close modal"} />
-					{imgFile.map((el) => {
-						console.log(el)
-						return <img src={`${LINK}/public/images/${page}/${el}`} key={el} alt='img' style={page == "Karusel" ? {width: "100%", height: "300px",  backgroundSize: '100% 100%', borderRadius: "10px"} : {} }/>
-					})}
+
+					<div  className={styles.butCon}>
+						<select name="listOfEvents" onChange={handleTitleChange} id={'select'}>
+						{
+							imgFile.map((el) => (
+								<>
+									<option value={`${Object.keys(el)[0]}`} key={`${Object.keys(el)[0]}`}>{Object.keys(el)[0]}</option>
+								</>
+
+							))
+						}
+						</select>
+
+						<button onClick={handleSubmit} className={stylesBut.filledBtn} style={{border : "none"}}>Delete</button>
+
+					</div>
+					{imgFile.map((el : {string : string[]}, i) => (
+						<div key={`${Object.keys(el)[0]}`} id={`${Object.keys(el)[0]}`} className={`${i == 0 ? '' : styles.hidden} ${styles.imgCont}`}>
+							{
+								Object.values(el)[0]?.map((img) =>(
+									<img src={`${LINK}/public/images/${page}/${Object.keys(el)[0]}/${img}`} key={`${Object.keys(el)[0]}-${img}`} id={`${Object.keys(el)[0]}/${img}`} style={{width: "300px"}} onClick={handleImgClick} className={`${styles.img}`}/>
+								))
+							}
+						</div>
+					))}
 					{/* Display Image Here */}
 				</div>
 			</>
--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,5 @@
 const LINK = "https://storage.sillamaesk.ee"
+//const LINK = "http://localhost:5000"
 
 
 export {LINK}
