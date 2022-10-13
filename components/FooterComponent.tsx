4e58dcfabc4febef62d47fb335d69f46dbba47a3
Ivan Niukanen <ivanniu2003@gmail.com>
2022-10-13 20:09:37 +0300

    footer translated to english

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -1,59 +1,128 @@
-import { observer } from 'mobx-react-lite'
-import styles from '../styles/FooterComponent.module.css'
-import LanguageStore from '../Stores/LanguageStore'
-import React, { useEffect, useState } from 'react'
-import { autorun } from 'mobx';
-import {useGetTimeTableQuery} from "../graphqlGenerated/graphql";
+import { observer } from "mobx-react-lite";
+import styles from "../styles/FooterComponent.module.css";
+import LanguageStore from "../Stores/LanguageStore";
+import React, { useEffect, useState } from "react";
+import { autorun } from "mobx";
+import { useGetFooterQuery, useGetTimeTableQuery } from "../graphqlGenerated/graphql";
 import languageStore from "../Stores/LanguageStore";
+import LanguageStoreV2 from "../Stores/LanguageStoreV2";
 
 function FooterComponent() {
-    const {data, loading, error} = useGetTimeTableQuery()
-    const lang = { 
-        contactUs: LanguageStore.currentLanguage.isEst ? LanguageStore.footer.ContactUs.EST : LanguageStore.footer.ContactUs.RUS,
-        sportComplex: LanguageStore.currentLanguage.isEst ? LanguageStore.footer.SportComplex.EST : LanguageStore.footer.SportComplex.RUS,
-        visitUs: LanguageStore.currentLanguage.isEst ? LanguageStore.footer.VisitUs.EST : LanguageStore.footer.VisitUs.RUS, 
-        folowUs: LanguageStore.currentLanguage.isEst ? LanguageStore.footer.FollowOnSocialMedia.EST : LanguageStore.footer.FollowOnSocialMedia.RUS
-    }
-
-    return(
-        <footer className={styles.footer}>
-            <div className={styles.footerInformation}>
-                <div className={styles.containerInformationFooter}>
-                    <h3>{lang.contactUs}</h3>
-                    <span>+372 392 4245</span>
-                    <span>+372 397 4077</span>
-                    <span>info@sillamaesk.ee</span>
-                </div>
-                <div className={styles.containerInformationFooter}>
-                    <h3>{lang.sportComplex}</h3>
-                    <span>Võta ühendust</span>
-                    <span>Spordikompleks Kalev</span>
-                    <span>Külasta meid</span>
-                </div>
-                <div className={styles.containerInformationFooter}>
-                    <h3>{lang.visitUs}</h3>
-                    <div className={styles.containerScheduleFooter}>
-                        { loading ? <p>Loading...</p> :
-                            <>
-                        <div>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SportComplex?.title?.EST :  data!.GetTimeTable?.SportComplex?.title?.RUS }</span>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SportComplex?.minTitle1?.EST :  data!.GetTimeTable?.SportComplex?.minTitle1?.RUS}: {data!.GetTimeTable?.SportComplex?.timeTable1}</span>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SportComplex?.minTitle2?.EST :  data!.GetTimeTable?.SportComplex?.minTitle2?.RUS}: {data!.GetTimeTable?.SportComplex?.timeTable2 }</span>
-                        </div>
-                        <div>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SwimmingPool?.title?.EST :  data!.GetTimeTable?.SwimmingPool?.title?.RUS }</span>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SwimmingPool?.minTitle1?.EST :  data!.GetTimeTable?.SwimmingPool?.minTitle1?.RUS}: {data!.GetTimeTable?.SwimmingPool?.timeTable1}</span>
-                            <span>{languageStore.currentLanguage.isEst ? data!.GetTimeTable?.SwimmingPool?.minTitle2?.EST :  data!.GetTimeTable?.SwimmingPool?.minTitle2?.RUS}: {data!.GetTimeTable?.SwimmingPool?.timeTable2 }</span>
-                        </div>
-
-                            </>
-                        }
-                    </div>
-                </div>
-            </div>
-            <a href={"https://www.facebook.com/spordikompleksKalev"} className={styles.facebookIcon} ></a>
-        </footer>
-    )
+	const { data, loading, error } = useGetTimeTableQuery();
+	const { data: footerData, loading: loadinfFooter } = useGetFooterQuery();
+	const lang = {
+		contactUs: LanguageStore.currentLanguage.isEst
+			? LanguageStore.footer.ContactUs.EST
+			: LanguageStore.footer.ContactUs.RUS,
+		sportComplex: LanguageStore.currentLanguage.isEst
+			? LanguageStore.footer.SportComplex.EST
+			: LanguageStore.footer.SportComplex.RUS,
+		visitUs: LanguageStore.currentLanguage.isEst
+			? LanguageStore.footer.VisitUs.EST
+			: LanguageStore.footer.VisitUs.RUS,
+		folowUs: LanguageStore.currentLanguage.isEst
+			? LanguageStore.footer.FollowOnSocialMedia.EST
+			: LanguageStore.footer.FollowOnSocialMedia.RUS,
+	};
+	const TimeTable = () => (
+		<div className={styles.containerScheduleFooter}>
+			{loading ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					<div>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SportComplex?.title?.EST
+								: data!.GetTimeTable?.SportComplex?.title?.RUS}
+						</span>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SportComplex?.minTitle1?.EST
+								: data!.GetTimeTable?.SportComplex?.minTitle1?.RUS}
+							: {data!.GetTimeTable?.SportComplex?.timeTable1}
+						</span>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SportComplex?.minTitle2?.EST
+								: data!.GetTimeTable?.SportComplex?.minTitle2?.RUS}
+							: {data!.GetTimeTable?.SportComplex?.timeTable2}
+						</span>
+					</div>
+					<div>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SwimmingPool?.title?.EST
+								: data!.GetTimeTable?.SwimmingPool?.title?.RUS}
+						</span>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SwimmingPool?.minTitle1?.EST
+								: data!.GetTimeTable?.SwimmingPool?.minTitle1?.RUS}
+							: {data!.GetTimeTable?.SwimmingPool?.timeTable1}
+						</span>
+						<span>
+							{languageStore.currentLanguage.isEst
+								? data!.GetTimeTable?.SwimmingPool?.minTitle2?.EST
+								: data!.GetTimeTable?.SwimmingPool?.minTitle2?.RUS}
+							: {data!.GetTimeTable?.SwimmingPool?.timeTable2}
+						</span>
+					</div>
+				</>
+			)}
+		</div>
+	);
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerInformation}>
+				<div className={styles.containerInformationFooter}>
+					{footerData?.GetFooter?.firstSection?.showHeader ? (
+						<h3>{footerData?.GetFooter?.firstSection?.header![LanguageStoreV2.currentLanguage] ?? ""}</h3>
+					) : (
+						<></>
+					)}
+					{footerData?.GetFooter?.firstSection?.showBody ? (
+						<span>{footerData?.GetFooter?.firstSection?.body![LanguageStoreV2.currentLanguage] ?? ""}</span>
+					) : (
+						<></>
+					)}
+				</div>
+				<div className={styles.containerInformationFooter}>
+					{footerData?.GetFooter?.secondSection?.showHeader ? (
+						<h3>{footerData?.GetFooter?.secondSection?.header![LanguageStoreV2.currentLanguage] ?? ""}</h3>
+					) : (
+						<></>
+					)}
+					{footerData?.GetFooter?.secondSection?.showBody ? (
+						<span>
+							{footerData?.GetFooter?.secondSection?.body![LanguageStoreV2.currentLanguage] ?? ""}
+						</span>
+					) : (
+						<></>
+					)}
+				</div>
+				<div className={styles.containerInformationFooter}>
+					{footerData?.GetFooter?.thirdSection?.showHeader ? (
+						<h3>{footerData?.GetFooter?.thirdSection?.header![LanguageStoreV2.currentLanguage] ?? ""}</h3>
+					) : (
+						<></>
+					)}
+					{footerData?.GetFooter?.thirdSection?.showBody ? (
+						footerData.GetFooter.showContacts ? (
+							<TimeTable />
+						) : (
+							<span>{footerData.GetFooter.thirdSection?.body![LanguageStoreV2.currentLanguage]}</span>
+						)
+					) : footerData?.GetFooter?.showContacts ? (
+						<TimeTable />
+					) : (
+						<></>
+					)}
+				</div>
+				<a href={"https://www.facebook.com/spordikompleksKalev"} className={styles.facebookIcon}></a>
+			</div>
+		</footer>
+	);
 }
 
-export default observer(FooterComponent)
+export default observer(FooterComponent);
